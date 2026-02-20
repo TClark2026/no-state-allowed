a62d4180d7d38a5e8b34744435202f2a4196ae73
Troy Clark <troyclark@OKDVTLFDYLF3.local>
2026-02-20 15:53:53 +0200

    data skeleton objects, more route fixing

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router"
 import Navbar from "./components/Navbar/Navbar"
 import Report from "./components/Report/Report"
 import Movies from "./components/Movies/Movies"
+import Landingpage from "./components/Landingpage/Landingpage"
+import Leaderboard from "./components/Leaderboard/Leaderboard"
 
 function App() {
 
@@ -10,10 +12,10 @@ function App() {
      <Navbar />
 
       <Routes>
-        <Route path="/" element={<App/>} />
+         <Route path="/" element={<Landingpage/>} />
         <Route path="/report" element={<Report/>} />
         <Route path="/movies" element={<Movies/>} />
-         <Route path="/leaderboard" element={<Movies/>} />
+         <Route path="/leaderboard" element={<Leaderboard/>} />
       </Routes>
     </>
   )
