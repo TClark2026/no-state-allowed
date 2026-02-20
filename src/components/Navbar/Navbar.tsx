a256c42eeeac848a739870ec17281baeb628588f
Troy Clark <troyclark@OKDVTLFDYLF3.local>
2026-02-20 16:31:17 +0200

    navbar styling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,13 @@
 import { NavLink } from "react-router-dom";
+import "./Navbar.scss"
 
 export default function Navbar() {
   return (
-    <nav style={{ display: "flex", gap: "20px", padding: "10px" }}>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/report">Report</NavLink>
-      <NavLink to="/movies">Movies</NavLink>
-    <NavLink to="/leaderboard">Leaderboard</NavLink>
+    <nav >
+      <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>Home</NavLink>
+      <NavLink to="/report" className={({ isActive }) => (isActive ? "active" : "")}>Report</NavLink>
+      <NavLink to="/movies" className={({ isActive }) => (isActive ? "active" : "")}>Movies</NavLink>
+      <NavLink to="/leaderboard" className={({ isActive }) => (isActive ? "active" : "")}>Leaderboard</NavLink>
     </nav>
   );
 }
